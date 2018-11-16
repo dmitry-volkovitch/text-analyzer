339b19fe2d7fc9a7c8474e97f2a5407b10c411a3
dima-volkovitch <dima.volkovitch@yandex.by>
2018-11-16 20:12:15 +0300

    Created a module to count the list of the most common words in the text.

---
+++ text-analyzator/word-counter/src/main/java/by/grsu/edu/word_counter/WordCounter.java
@@ -0,0 +1,60 @@
+package by.grsu.edu.word_counter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+/**
+ * Class that counts and stores the list of top-10 most common words in the file.
+ * @author dimav
+ *
+ */
+public class WordCounter {
+	private WordReader reader;
+	
+	private List<WordStorage> wordList;
+
+	private List<WordStorage> result;
+	
+	public WordCounter(WordReader reader) {
+		this.reader = reader;
+		wordList = new ArrayList<>();
+	}
+	
+	/**
+	 * Count top-10 most common words in the file.
+	 * @throws IOException
+	 */
+	public void count() throws IOException {
+		StringBuilder str = null;
+		while((str = reader.readWord()) != null) {
+			WordStorage storage = new WordStorage(str, 1);
+			if(wordList.contains(storage)) {
+				int i = wordList.indexOf(storage);
+				storage = wordList.get(i);
+				storage.setRepetitionsNumber(storage.getRepetitionsNumber() + 1);
+				continue;
+			}
+			wordList.add(storage);
+		}
+		
+		result = wordList.stream().sorted(new Comparator<WordStorage>() {
+
+			@Override
+			public int compare(WordStorage o1, WordStorage o2) {
+				
+				return o2.getRepetitionsNumber() - o1.getRepetitionsNumber();
+			}
+		}).collect(Collectors.toList()).subList(0, 10);
+		
+	}
+	
+	/**
+	 * 
+	 * @return return the list of top-10 most common words in the file.
+	 */
+	public List<WordStorage> getResult() {
+		return result;
+	}
+}
